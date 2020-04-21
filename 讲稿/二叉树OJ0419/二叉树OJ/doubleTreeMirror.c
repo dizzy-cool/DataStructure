abcaaac3c3a2c5719c0cd2446a0906f5db8255b3
dizzy-cool <965136370@qq.com>
2020-04-21 12:33:24 +0800

    二叉树找镜像,找错

--- 讲稿/二叉树OJ0419/二叉树OJ/doubleTreeMirror.c
+++ 讲稿/二叉树OJ0419/二叉树OJ/doubleTreeMirror.c
@@ -1,19 +1,18 @@
-bool isSy(struct TreeNode* p, struct TreeNode* q) {
-    //
-    if (p == NULL && q == NULL) {
+bool isSy(struct TreeNode* p, struct TreeNode* q){
+    //同时走到空， 结构相同
+    if(q == NULL && p == NULL)
         return true;
-    }
-    if (p == NULL && q == NULL) {
+    //没有同时走到空， 结构不同
+    if(q == NULL || p == NULL)
         return false;
-    }
+    // 对应位置的值以及子结构是否相同
     return p->val == q->val
-        && isSy(p->left, q->right)
-        && isSy(p->right, q->left);
+    && isSy(p->left, q->right)
+    && isSy(p->right, q->left);
 }
 
 bool isSymmetric(struct TreeNode* root){
-    if (root == NULL) {
+    if(root == NULL)
         return true;
-    }
     return isSy(root->left, root->right);
 }
